18734c709ea2f48dc948570855bb6a5203787918
makethink45-code <makethink45@gmail.com>
2026-03-29 23:20:28 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBd83Jk5n7M2mkumtFT-t_zktD8Wz0cZnM",
+  authDomain: "stockmaster-94534.firebaseapp.com",
+  projectId: "stockmaster-94534",
+  storageBucket: "stockmaster-94534.firebasestorage.app",
+  messagingSenderId: "520506980567",
+  appId: "1:520506980567:web:e5d7661a3866d18d979892"
+};
